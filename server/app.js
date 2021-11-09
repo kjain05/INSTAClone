d49615cef40d9ae67de1f2f1fa8b9e8e51f5e9f3
kjain05 <kartikeyjain2001@gmail.com>
2021-11-09 18:05:26 +0530

    Added user schema in models folder

--- server/app.js
+++ server/app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
+const port = 5000;
+const mongoose = require('mongoose');
+require('./models/user');
+
 // KREyYPJr3q!n.YN
+
+
+app.listen(port, () => {
+
+    console.log("SERVER running on ", port);
+});
