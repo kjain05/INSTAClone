cfc8e6657b64028cdea8d0579b5382f7ce7c1c7b
SURAJ JHA <sarcasticsuraj7048@gmail.com>
2021-11-10 22:29:53 +0530

    testing

--- server/app.js
+++ server/app.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
+const {MONGOURI} = require('./keys');
 require('./models/user');
 
-// KREyYPJr3q!n.YN
+//parse
+app.use(express.json());
+//requiring router file
+app.use(require('./route/app'));
 
+//connecting to mogoose database
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo successfully!");
+})
+
+//logging  error on console
+mongoose.connection.on('error',(err)=>{
+    console.log("sorry,can't connect!",err);
+})
+
+// KREyYPJr3q!n.YN
+//database user password(username: Suraj) PwgUP0nej6DFMsji
 
 app.listen(port, () => {
 
