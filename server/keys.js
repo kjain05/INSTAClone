c57e1f2bee03d2c92f7c353e5e9cc8473f8bf6aa
kjain05 <kartikeyjain2001@gmail.com>
2021-11-11 21:36:21 +0530

    changed keys.js

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,3 @@
-module.exports={
-    MONGOURI : "mongodb+srv://Suraj:PwgUP0nej6DFMsji@Insagramcluster.8z1zz.mongodb.net/insagramClone?retryWrites=true&w=majority"
+module.exports = {
+    MONGOURI: "mongodb+srv://instaDB:<76BLErXSc1hbhmjt>@cluster0.wmmah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
