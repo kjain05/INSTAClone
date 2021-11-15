f40768250e8c7b51c0386a75dde5fbe39da69859
kjain05 <kartikeyjain2001@gmail.com>
2021-11-15 22:34:09 +0530

    updated app.js according to recent file changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import "./App.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/screens/Home'
+import Signup from './components/screens/Signup'
+import Signin from './components/screens/Signin'
+import Profile from './components/screens/Profile'
 function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+
+    </BrowserRouter>
+
   );
 }
 
